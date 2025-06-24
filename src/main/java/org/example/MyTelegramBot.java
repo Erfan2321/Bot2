a01581b5c4b0f355e9e08b8b45fb6f2a8749f6c8
erfan <Erfansedaghat2321@gmail.com>
2025-06-24 11:53:09 +0330

    Initial commit

--- src/main/java/org/example/MyTelegramBot.java
+++ src/main/java/org/example/MyTelegramBot.java
@@ -1,40 +1,28 @@
 package org.example;
 
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.util.ArrayList;
-
 
 public class MyTelegramBot extends TelegramLongPollingBot {
 
-    public void onUpdateReceived(org.telegram.telegrambots.meta.api.objects.Update update) {
+    public MyTelegramBot() {
+        super(new DefaultBotOptions());
+    }
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-
+    @Override
+    public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
+            System.out.println("Received: " + messageText);
+
             SendMessage message = new SendMessage();
             message.setChatId(chatId);
-            int number;
-
-            if (messageText.trim().equalsIgnoreCase("average")) {
-                message.setText("" + calculateAvg(numbers));
-                numbers.clear();
-            }
-            else {
-                try {
-                    number = Integer.parseInt(messageText);
-                    numbers.add(number);
-
-                } catch (Exception e) {
-                    message.setText("Wrong entry");
-                }
-            }
+            message.setText(messageText);
 
             try {
                 execute(message);
@@ -43,18 +31,14 @@ public class MyTelegramBot extends TelegramLongPollingBot {
             }
         }
     }
-    private int calculateAvg (ArrayList<Integer> numbers) {
 
-        int sum = 0;
-        for (Integer integer : numbers)
-            sum += integer;
-
-        return sum/numbers.size();
-    }
+    @Override
     public String getBotUsername() {
-        return "MyTestJavaBot123_bot";
+        return "MyTestJavaBot222_bot";
     }
+
+    @Override
     public String getBotToken() {
-        return "7661881146:AAEujXxenoQI7msIWRp4a93bV3h_4Soh0po";
+        return "7999300553:AAG_8GqNWONP1W8Y_OP6XG4cLBUqglN89CI";
     }
 }
