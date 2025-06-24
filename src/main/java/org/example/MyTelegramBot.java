34916eb3273989ad209237038cc84f8d8523561a
erfan <Erfansedaghat2321@gmail.com>
2025-06-24 20:36:40 +0330

    Initial commit

--- src/main/java/org/example/MyTelegramBot.java
+++ src/main/java/org/example/MyTelegramBot.java
@@ -4,7 +4,12 @@ import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyTelegramBot extends TelegramLongPollingBot {
 
@@ -18,11 +23,27 @@ public class MyTelegramBot extends TelegramLongPollingBot {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
-            System.out.println("Received: " + messageText);
-
             SendMessage message = new SendMessage();
             message.setChatId(chatId);
-            message.setText(messageText);
+
+            switch (messageText) {
+                case "/start":
+                    message.setText("یکی از گزینه‌های زیر رو انتخاب کن:");
+                    message.setReplyMarkup(getMainKeyboard());
+                    break;
+
+                case "🔢 عدد":
+                    message.setText("لطفاً یک عدد وارد کن:");
+                    break;
+
+                case "🔤 کلمه":
+                    message.setText("لطفاً یک کلمه وارد کن:");
+                    break;
+
+                default:
+                    message.setText("دریافت شد: " + messageText);
+                    break;
+            }
 
             try {
                 execute(message);
@@ -32,11 +53,25 @@ public class MyTelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private ReplyKeyboardMarkup getMainKeyboard() {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true); // بهتره برای موبایل
+
+        KeyboardRow row = new KeyboardRow();
+        row.add("🔢 عدد");
+        row.add("🔤 کلمه");
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row);
+
+        keyboardMarkup.setKeyboard(keyboard);
+        return keyboardMarkup;
+    }
+
     @Override
     public String getBotUsername() {
         return "MyTestJavaBot222_bot";
     }
-
     @Override
     public String getBotToken() {
         return "7999300553:AAG_8GqNWONP1W8Y_OP6XG4cLBUqglN89CI";
